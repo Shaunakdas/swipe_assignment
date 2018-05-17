531ebebfaff99266c2a050978e74032294f82528
Shaunakdas <shaunakdas2020@gmail.com>
2018-05-17 20:40:16 +0530

    Upper Card ui done

--- components/Deck.js
+++ components/Deck.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
 import { 
-  View
+  TouchableOpacity, View, Animated, PanResponder,
+  Dimensions, LayoutAnimation, UIManager, Text
 } from 'react-native';
+import { Card, Button } from 'react-native-elements';
 
 
 class Deck extends Component {
+
+	renderUpperCard() {
+    return (
+      <Card
+        image={{ }}
+        title={`Card #1`}
+      >
+        <Text>
+          I can customize the Card further.
+        </Text>
+        <TouchableOpacity
+          style={styles.button} >
+           <Text>View Now!</Text>
+        </TouchableOpacity>
+      </Card>
+    );
+  }
+
 	render() {
     return (
       <View>
-        
+        {this.renderUpperCard()}
       </View>
     );
   }
 }
 
 const styles = {
-  
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    padding: 10
+  },
 };
 
 export default Deck;
